b4e4a790c1b0ec6746ea32a28529cab89e18a978
Ashley <ajychen26@gmail.com>
2023-06-23 14:25:04 -0700

    small fix

--- trace-symexec/run_demo.sh
+++ trace-symexec/run_demo.sh
@@ -4,4 +4,4 @@
 
 # python3 symexec.py MultiVulnToken2.sol theorem2.json trace2.txt
 
-curl -H 'Content-Type: application/json' --data '{"jsonrpc":"2.0", "id": 1, "method": "debug_traceTransaction", "params": ["0x4e44d023f6bc1388bb8fdcdb6908a1777bbf586bd8f94e2f6911ae87a1537ddd",{} ] }' http://localhost:7545 | json_pp > trace_no_reentrancy.jso
+curl -H 'Content-Type: application/json' --data '{"jsonrpc":"2.0", "id": 1, "method": "debug_traceTransaction", "params": ["0x4e44d023f6bc1388bb8fdcdb6908a1777bbf586bd8f94e2f6911ae87a1537ddd",{} ] }' http://localhost:7545 | json_pp > trace_no_reentrancy.json
