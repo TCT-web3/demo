056d79208fd218c7eea895c1f298eb759017b97d
Shuo Chen (MSR) <shuochen@microsoft.com>
2023-06-25 15:03:23 -0700

    Change memory offset from hex string to int. Progress on misaligned mload

--- trace-symexec/symexec.py
+++ trace-symexec/symexec.py
@@ -253,7 +253,7 @@ modifies balances;
                         s = hex(temp[key].value)
                     else:
                         s = temp[key]
-                    print(key, ": ", s)
+                    print(hex(key), ": ", s)
         elif what == "storage":
             print("-----Storage-----")
             for key in self._storage:
@@ -313,7 +313,7 @@ modifies balances;
                 return 0,separation_position-1
         else:
             return -1,-1  #a is not a 32-byte mask
-        
+    '''    
     def count_lower_00s(self, a):
         if a == 0:
             return -1
@@ -322,12 +322,76 @@ modifies balances;
             c += 1
             a //= 0x100
         return c, a
-    def run_instruction(self, instr, branch_taken):
-        # self.inspect("stack")
-        print(instr)
- 
-        # self.inspect("memory")
+    '''    
+    def mem_item_len(self, mem_item):
+        if mem_item.value != "Partial32B":
+            return 32
+        segment = mem_item.children[0]
+        return segment[1]-segment[0]+1
         
+    def handle_mload(self, mem_offset):
+        prev_k=None
+        in_copy_mode = false
+        node = SVT("concat")
+        bytes_to_copy = 32
+        for k,v in self._memories[self._curr_contract].items():
+            if k == mem_offset:
+                return self._memories[self._curr_contract][hex(mem_offset.value)]
+            if in_copy_mode:
+                if bytes_to_copy == 0:
+                    return node
+                elif bytes_to_copy < 0:
+                    raise Exception("number of remaining bytes < 0!") 
+                else:
+                    pass
+            elif k > mem_offset:   
+            # We have found the place to insert the mem item of mem_offset. It is between prev_k and k
+                prev_len = mem_item_len(prev_v)
+                if (mem_offset - prev_k < prev_len):
+                #In this case, mem_offset falls into the previous item.
+                    ignored_len = mem_offset - prev_k
+                    len_to_copy = prev_len - ignored_len
+                    node1=SVT("Partial32B")
+                    if prev_v!= "Partial32B":
+                        node1_segment = (ignored_len,31)
+                        node1_value = prev_v
+                    else:
+                        original_segment = prev_v.children[0]
+                        node1_segment = (original_segment[0]+ignored_len, original_segment[1])
+                        node1_value = prev_v.children[1]
+                    node1.children.append(node1_segment)
+                    node1.children.append(node1_value)
+                    node.children.append(node1)
+                    bytes_to_copy -= len_to_copy
+                    unfilled_position = prev_k + prev_len
+                else:
+                    unfilled_position = mem_offset
+                
+                #We handle the bytes between unfilled_position and k
+                len_uninitialized_bytes = k-unfilled_position
+                if len_uninitialized_bytes >= 32 and bytes_to_copy==32:                   
+                    node.children.append(SVT(0))
+                    bytes_to_copy = 0
+                    break
+                elif len_uninitialized_bytes > 0:
+                    node1=SVT("Partial32B")
+                    num_zero_bytes = min(len_uninitialized_bytes,bytes_to_copy)
+                    node1.children.append(0,num_zero_bytes-1)
+                    node1.children.append(SVT(0))
+                    node.children.append(node1)
+                    bytes_to_copy-=num_zero_bytes
+                #start the copy mode
+                in_copy_mode = true
+            prev_k = k
+            prev_v = v
+                
+        if immediate_next==None:   
+        # This is an uninitialized space. It is zero according to EVM's spec
+            return SVT(0)
+            
+        
+    
+    def run_instruction(self, instr, branch_taken):
         PC=instr[0]
         opcode=instr[1]
         operand=instr[2]
@@ -341,11 +405,12 @@ modifies balances;
         #     print("=======before======")
         #     self.inspect("memory")
         #     self.inspect("stack")
-        # if int(PC) >= 825 and int(PC) <= 830:
-        #     print("=======before======")
-        #     self.inspect("memory")
-        #     self.inspect("stack")
-
+        if int(PC)==829 or int(PC) >= 1749 and int(PC) <= 1775:
+            print("=======before======")
+            self.inspect("memory")
+            self.inspect("stack")
+        print(instr)
+        
         if instr[0]==(">"):
             # self.inspect("memory")
             # self.inspect("stack")
@@ -356,10 +421,10 @@ modifies balances;
 
 
             if (dest_contract not in self._stacks.keys()):
-                offset = self._memories[self._curr_contract][hex(self._stacks[self._curr_contract][-4].value)]
+                offset = self._memories[self._curr_contract][self._stacks[self._curr_contract][-4].value]
                 length = self._stacks[self._curr_contract][-5]
                 self._stacks[dest_contract] = self.set_callStack(offset, length)  
-                self._memories[dest_contract] = {}
+                self._memories[dest_contract] = self.set_memory()
 
              # pops out the operands for a successful CALL operation
             for i in range(7):
@@ -412,7 +477,7 @@ modifies balances;
             # mem_offset //= 32 #   use actually offset
             value = self._stacks[self._curr_contract].pop()
             if not isinstance(mem_offset.value, int):
-                self._memories[self._curr_contract][str(mem_offset)] = value
+                self._memories[self._curr_contract][mem_offset] = value
             else:
                 if not mem_offset.value in self._memories[self._curr_contract].keys() and value.value == "OR":
                     rl = value.children[0]
@@ -430,24 +495,30 @@ modifies balances;
                     if ffs != OOs:
                         raise Exception("MSTORE exception")
                     print(hex(a))
-                    self._memories[self._curr_contract][hex(mem_offset.value)] = SVT(a) 
+                    self._memories[self._curr_contract][mem_offset.value] = SVT(a) 
 
                 else:
-                    self._memories[self._curr_contract][hex(mem_offset.value)] = value   
+                    self._memories[self._curr_contract][mem_offset.value] = value   
             self._memories[self._curr_contract] = dict(sorted(self._memories[self._curr_contract].items()))  # use sorted dictionary to mimic memory allocation  
             
         elif opcode=="MLOAD":
             mem_offset = self._stacks[self._curr_contract].pop()
             if not isinstance(mem_offset.value, int):
+                '''
                 if mem_offset not in self._memories[self._curr_contract].keys():
                     value = 0 # empty memory space
                 else:    
                     value = self._memories[self._curr_contract][str(mem_offset)] # get symbolic memory location 
+                '''
+                raise Exception("Memory offset is not a concrete value. CodeGen is needed here!")
             else:
+                '''
                 if hex(mem_offset.value) not in self._memories[self._curr_contract].keys():
                     value = SVT("unknown") # empty memory space
                 else:   
                     value = self._memories[self._curr_contract][hex(mem_offset.value)] # get exact memory location
+                '''
+                value = self.handle_mload(mem_offset)
             self._stacks[self._curr_contract].append(value)  
         elif opcode=="SSTORE":
             # print(instr)
@@ -494,20 +565,20 @@ modifies balances;
         elif opcode=="AND":
             a = self._stacks[self._curr_contract].pop()
             b = self._stacks[self._curr_contract].pop()
-            mask = None
+            segment = None
             if isinstance(a.value, int):
                 first,last = self.recognize_32B_mask(a.value)
                 if first!=-1:
-                    mask = (first,last)
+                    segment = (first,last)
                     num = b
             elif isinstance(b.value, int):
                 first,last = self.recognize_32B_mask(b.value)
                 if first!=-1:
-                    mask = (first,last)
+                    segment = (first,last)
                     num = a
-            if mask != None:
-                node = SVT("Masked32B")
-                node.children.append(SVT(mask))
+            if segment != None:
+                node = SVT("Partial32B")
+                node.children.append(SVT(segment))
                 node.children.append(num)
             else:
                 if isinstance(a.value, int) and isinstance(b.value, int):
@@ -553,8 +624,8 @@ modifies balances;
                 # node.children.append(self._memory[start_offset//32+1]) # map name: balances
                 # node.children.append(self._memory[start_offset//32]) # key in balances
 
-                node.children.append(self._memories[self._curr_contract][hex(start_offset+32)])
-                node.children.append(self._memories[self._curr_contract][hex(start_offset)])
+                node.children.append(self._memories[self._curr_contract][start_offset+32])
+                node.children.append(self._memories[self._curr_contract][start_offset])
 
                 self._stacks[self._curr_contract].pop() # pop 64
                 self._stacks[self._curr_contract].append(node)
@@ -618,7 +689,11 @@ def set_storage():
 
 def set_memory():
     return {
-        hex(64): SVT(128) # initial setting of the memory!!! 
+        # Why does this mem item exist?
+        # hex(64): SVT(128) # initial setting of the memory!!! 
+        
+        # Dummy mem item
+        0xffff: SVT(0)
     }
 
 def read_path(filename):
