c064ad00137a007ddac9319b1b0b7f189b2761c5
Ashley <ajychen26@gmail.com>
2023-06-30 14:40:41 -0700

    file starts at entry point

--- web-demo/convert_trace.py
+++ web-demo/convert_trace.py
@@ -13,14 +13,12 @@ def output_trace(trace_fname, tx_hash, deployment_fname, theorem_fname):
     output = open("trace-" + tx_hash + ".txt", "w")
 
     # Map entry point to deployment file
-    hashes = re.search(r"([^:]+)::([^:]+)", theorem["entry-for-real"])
+    hashes = re.search(r"0x([^:]+)::0x([^:]+)", theorem["entry-for-real"])
     contract_hash = hashes.group(1)
     function_hash = hashes.group(2)
-    print(contract_hash, function_hash)
     names = re.search(r"([^:]+)::([^:]+)", theorem["entry-for-test"])
     contract_name = names.group(1)
     function_name = names.group(2)
-    print(contract_name, function_name)
 
     deploy_info[contract_hash] = contract_name
     deploy_info[function_hash] = function_name
@@ -30,7 +28,8 @@ def output_trace(trace_fname, tx_hash, deployment_fname, theorem_fname):
 
     # Write header
     output.write("======================================Begin==========================================\n")
-    output.write(">>enter " + contract_hash + "::" + function_hash + " (" + contract_name + "::" + function_name + ")\n")
+    output.write(">>enter 0x" + contract_hash + "::0x" + function_hash + " (" + contract_name + "::" + function_name + ")\n")
+    enter = False
     # Write rest of trace
     for i in range(len(trace)):
         line = ""
@@ -49,15 +48,24 @@ def output_trace(trace_fname, tx_hash, deployment_fname, theorem_fname):
         # Call
         elif opcode == "CALL":
             # hex
-            contract_address = trace[i]["stack"][-2]
-            line += "-\n>>enter " + '0x' + contract_address
+            contract_address = trace[i]["stack"][-2].lstrip("000000000000000000000000")
             offset = int(trace[i]["stack"][-4], 16)
             row = offset // 32
             col = (offset % 32)*2
             func_selector = trace[i]["memory"][row][col:col+8]
-            line += "::0x" + func_selector
-            # deployment info
-            line += "(" + deploy_info[contract_address] + "::" + deploy_info[func_selector] + ") "
+
+            if not enter and func_selector == function_hash:
+                output.close()
+                output = open("trace-" + tx_hash + ".txt", "w")
+                output.write("======================================Begin==========================================\n")
+                line = ">>enter 0x" + contract_hash + "::0x" + function_hash + " (" + deploy_info[contract_hash] + "::" + deploy_info[function_hash] + ") "
+                enter = True
+
+            else:
+                line += "-\n>>enter " + '0x' + contract_address
+                line += "::0x" + func_selector
+                # deployment info
+                line += " (" + deploy_info[contract_address] + "::" + deploy_info[func_selector] + ") "
         elif opcode == "RETURN":
             line += "-\n<<leave "
         elif opcode == "STOP":
@@ -66,3 +74,15 @@ def output_trace(trace_fname, tx_hash, deployment_fname, theorem_fname):
     output.write("======================================End==========================================")
     trace_file.close()
     output.close()
+
+def main():
+    # Get file names
+    ARGS = sys.argv
+    TRACE_FNAME = ARGS[1]
+
+    # Call method
+    output_trace(TRACE_FNAME, "0xdc796b5fd6d87794379e0014ad3dfe406696a6deda3c6fe45844225589a75722", "deployment_info.json", "theorem-0xdc796b5fd6d87794379e0014ad3dfe406696a6deda3c6fe45844225589a75722.json")
+    
+
+if __name__ == "__main__":
+    main()
