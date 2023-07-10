eafb818a1e41df8a71777a46e1baebafcc99a027
Shuo Chen (MSR) <shuochen@microsoft.com>
2023-07-10 11:39:43 -0700

    add everything.sol

---
+++ uniswapv2-solc0.8/everything.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.4;
+
+import "./UniswapV2ERC20.sol";
+import "./UniswapV2Factory.sol";
+import "./UniswapV2Pair.sol";
+import "./UniswapV2Router.sol";
+import "./tokens/FancyToken.sol";
+
+import "./test.sol";
+
+contract Dummy {
+        
+}
