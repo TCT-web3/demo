44c2dd373cf6940ab5e21e9c0b4c2795178aab62
Ashley <ajychen26@gmail.com>
2023-07-31 15:38:37 -0700

    new files for run demo

--- web-demo/run_demo.sh
+++ web-demo/run_demo.sh
@@ -6,14 +6,21 @@ BOOGIE=boogie
 
 ### DEMO: addLiquidity
 echo "--------------------(Demo 1)------------------------"
-SOLIDITY=../uniswapv2-solc0.8/contract_everything.sol
-THEOREM=../uniswapv2-solc0.8/theorem_addLiq.json
-TRACE=trace_UniswapAddLiquidity2.txt
+SOLIDITY=../uniswapv2-solc0.8-new/test.sol
+# THEOREM=../uniswapv2-solc0.8/theorem_addLiq.json
+# TRACE=trace_UniswapAddLiquidity2.txt
 python3 ${SYMEXEC} ${SOLIDITY} ${THEOREM} ${TRACE}
 ${BOOGIE} /proverOpt O:smt.arith.solver=2 TCT_out_addLiq.bpl
 echo ""
 
 
+# python3 web-demo/src/symexec.py uniswapv2-solc0.8-new/test.sol web-demo/uploads/theorem_addLiquidity.json web-demo/trace_addLiquidity.txt
+# python3 web-demo/src/symexec.py uniswapv2-solc0.8-new/test.sol web-demo/uploads/theorem_removeLiquidity.json web-demo/trace_removeLiquidity.txt
+# python3 web-demo/src/symexec.py uniswapv2-solc0.8-new/test.sol web-demo/uploads/theorem_swap.json web-demo/trace_swap.txt
+
+# ${BOOGIE} /proverOpt O:smt.arith.solver=2 addLiqTest.bpl
+# ${BOOGIE} /proverOpt O:smt.arith.solver=2 removeLiqTest.bpl
+# ${BOOGIE} /proverOpt O:smt.arith.solver=2 swapTest.bpl
 
 
 
