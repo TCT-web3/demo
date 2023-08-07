47440e20561fa157a6f265d902d4bae26ba7b53c
Ashley <ajychen26@gmail.com>
2023-08-07 15:53:36 -0700

    new FancyToken traces

--- uniswapv2-solc0.8-new/tokens/FancyToken.sol
+++ uniswapv2-solc0.8-new/tokens/FancyToken.sol
@@ -16,16 +16,22 @@ contract FancyToken is IERC20 {
         balanceOf[msg.sender] = totalSupply;
     }
     function transfer(address recipient, uint amount) external override returns (bool) {
-        balanceOf[msg.sender] -= amount;
-        balanceOf[recipient] += amount;
-        //emit Transfer(msg.sender, recipient, amount);
-        return true;
+        unchecked {
+            require(balanceOf[msg.sender] >= amount);
+            balanceOf[msg.sender] -= amount;
+            balanceOf[recipient] += amount;
+            //emit Transfer(msg.sender, recipient, amount);
+            return true;
+        }
     }
 
     function approve(address spender, uint amount) external override returns (bool) {
-        allowance[msg.sender][spender] = amount;
-        //emit Approval(msg.sender, spender, amount);
-        return true;
+        unchecked {
+            allowance[msg.sender][spender] = amount;
+            //emit Approval(msg.sender, spender, amount);
+            return true;
+        }
+    
     }
 
     function transferFrom(
@@ -33,22 +39,31 @@ contract FancyToken is IERC20 {
         address recipient,
         uint amount
     ) external override returns (bool) {
-        allowance[sender][msg.sender] -= amount;
-        balanceOf[sender] -= amount;
-        balanceOf[recipient] += amount;
-        //emit Transfer(sender, recipient, amount);
-        return true;
+        unchecked {
+            require(balanceOf[sender] >= amount);
+            allowance[sender][msg.sender] -= amount;
+            balanceOf[sender] -= amount;
+            balanceOf[recipient] += amount;
+            //emit Transfer(sender, recipient, amount);
+            return true;
+        }
+        
     }
 
     function mint(uint amount) external {
-        balanceOf[msg.sender] += amount;
-        totalSupply += amount;
-        //emit Transfer(address(0), msg.sender, amount);
+        unchecked {
+            balanceOf[msg.sender] += amount;
+            totalSupply += amount;
+            //emit Transfer(address(0), msg.sender, amount);
+        }
     }
 
     function burn(uint amount) external {
-        balanceOf[msg.sender] -= amount;
-        totalSupply -= amount;
-        //emit Transfer(msg.sender, address(0), amount);
+        unchecked {
+            require(balanceOf[msg.sender] >= amount);
+            balanceOf[msg.sender] -= amount;
+            totalSupply -= amount;
+            //emit Transfer(msg.sender, address(0), amount);
+        }
     }
 }
